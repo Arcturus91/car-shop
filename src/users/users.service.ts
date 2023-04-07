d900d22503dcc927460e8349fabd86c052dae34e
Arcturus91 <arturo.barrantesv@pucp.pe>
2023-04-07 00:16:55 -0500

    Implement Report and User repository and DB query methods

---
+++ src/users/users.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    return this.repo.findOne({ where: { id } });
+  }
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+  update(id: number) {}
+  remove() {}
+}
